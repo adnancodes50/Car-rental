a7d68c58fa9942db38ea5bae3ce1b0c4edad896a
adnancodes50 <adnan.codes50@gmail.com>
2025-10-24 14:16:53 +0500

    chnages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,11 +15,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
     public function boot(): void
     {
         Schema::defaultStringLength(191);
-
         Blade::if('permission', function (string $permissionName) {
             if (auth()->user()->email == config('starter.super_admin_email')) {
                 return true;
@@ -34,12 +32,10 @@ class AppServiceProvider extends ServiceProvider
             $permissions = array_unique($permissions);
             return in_array($permissionName, $permissions);
         });
-
         Blade::if('onlydev', function () {
             $possibleEnvs = ['local', 'dev', 'development'];
             return in_array(config('app.env'), $possibleEnvs);
         });
-
         try {
             $detail = Cache::remember('project_brand_cache', 60, function () {
                 if (!Schema::hasTable('projects_details') && !Schema::hasTable('project_models')) {
@@ -47,33 +43,22 @@ class AppServiceProvider extends ServiceProvider
                 }
                 return ProjectModel::select(['project_name', 'logo'])->first();
             });
-
             $title = $detail->project_name ?? 'AdminLTE';
             $logoPath = (!empty($detail?->logo))
                 ? 'storage/' . ltrim($detail->logo, '/')
                 : 'vendor/adminlte/dist/img/logo.png';
-
             Config::set('adminlte.title', $title);
-
             Config::set('adminlte.logo', e($title));
-
             Config::set('adminlte.logo_img', $logoPath);
             Config::set('adminlte.logo_img_class', 'brand-image brand-image-custom');
-
-
             Config::set('adminlte.auth_logo.enabled', true);
             Config::set('adminlte.auth_logo.img.path', $logoPath);
             Config::set('adminlte.auth_logo.img.alt', $title);
             Config::set('adminlte.auth_logo.img.width', 80);
             Config::set('adminlte.auth_logo.img.height', 40);
             Config::set('adminlte.auth_logo.img.class', 'auth-logo-rounded');
-
-
-
-Config::set('adminlte.use_full_favicon', true);
-Config::set('adminlte.use_ico_only', true);
-
-
+            Config::set('adminlte.use_full_favicon', true);
+            Config::set('adminlte.use_ico_only', true);
             Config::set('adminlte.preloader.enabled', true);
             Config::set('adminlte.preloader.img.path', $logoPath);
             Config::set('adminlte.preloader.img.alt', $title);
